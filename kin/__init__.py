7e334b7ee4596766213a44ebffe8cb509078eff2
David Bolshoy <bolshoy@gmail.com>
2018-04-09 15:41:00 +0300

    exported account and transaction models [skip ci]

--- kin/__init__.py
+++ kin/__init__.py
@@ -1,5 +1,5 @@
 from .sdk import SDK
 from .config import *
 from .errors import *
-#from .models import AccountData, TransactionData
+from .stellar.horizon_models import AccountData, TransactionData
 from .version import __version__
